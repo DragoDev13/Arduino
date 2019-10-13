d1efd8f1b393028bb1f6b8a72fe68dfe1cd1f3d7
DragoDev13 <56112211+DragoDev13@users.noreply.github.com>
2019-10-13 16:51:14 +0200

    Update CD4511BE_3x7seg.ino

--- CD4511BE_3x7seg.ino
+++ CD4511BE_3x7seg.ino
@@ -13,7 +13,7 @@ int katodaCifraTri = 8;
  dole desno 4
  srednja 5
  gornja levo 9
- gornja desno 7
+ gornja desno 7 
  gornja srednja 10
  */
 void setup() {
@@ -33,6 +33,20 @@ void setup() {
   digitalWrite(katodaCifraTri, HIGH);
  
 }
+void decToBin(int broj){
+  if(broj%2==1)digitalWrite(BD1, HIGH);
+  else digitalWrite(BD1, LOW);
+  broj = broj / 2;
+  if(broj%2==1)digitalWrite(BD2, HIGH);
+  else digitalWrite(BD2, LOW);
+  broj = broj / 2;
+  if(broj%2==1)digitalWrite(BD3, HIGH);
+  else digitalWrite(BD3, LOW);
+  broj = broj / 2;
+  if(broj%2==1)digitalWrite(BD4, HIGH);
+  else digitalWrite(BD4, LOW);
+  broj = broj / 2;
+}
 void ispisNaDispleju(int broj){
     int temp;
     digitalWrite(BD1, LOW);
@@ -47,113 +61,40 @@ void ispisNaDispleju(int broj){
     else brojVeciOd99(broj);
 }
 void brojManjiOd10(int broj){
-    int temp;
     digitalWrite(katodaCifraTri, LOW);
     delay(1);
     digitalWrite(katodaCifraTri, HIGH);
     digitalWrite(katodaCifraDva, LOW);
     delay(1);
     digitalWrite(katodaCifraDva, HIGH);
-    temp = broj;
-    if(temp%2==0)digitalWrite(BD1, LOW);
-    else digitalWrite(BD1, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD2, LOW);
-    else digitalWrite(BD2, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD3, LOW);
-    else digitalWrite(BD3, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD4, LOW);
-    else digitalWrite(BD4, HIGH);
+    decToBin(broj);
     digitalWrite(katodaCifraJedan, LOW);
     delay(1);
     digitalWrite(katodaCifraJedan, HIGH);
 }
 void brojManjiOd100(int broj){
-    int temp;
     digitalWrite(katodaCifraTri, LOW);
     delay(1);
     digitalWrite(katodaCifraTri, HIGH);
-
-    temp = broj/10;
-    if(temp%2==0)digitalWrite(BD1, LOW);
-    else digitalWrite(BD1, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD2, LOW);
-    else digitalWrite(BD2, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD3, LOW);
-    else digitalWrite(BD3, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD4, LOW);
-    else digitalWrite(BD4, HIGH);
+    decToBin(broj/10);
     digitalWrite(katodaCifraDva, LOW);
     delay(1);
     digitalWrite(katodaCifraDva, HIGH);
-
-    temp = broj%10;
-    if(temp%2==0)digitalWrite(BD1, LOW);
-    else digitalWrite(BD1, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD2, LOW);
-    else digitalWrite(BD2, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD3, LOW);
-    else digitalWrite(BD3, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD4, LOW);
-    else digitalWrite(BD4, HIGH);
+    decToBin(broj%10);
     digitalWrite(katodaCifraJedan, LOW);
     delay(1);
     digitalWrite(katodaCifraJedan, HIGH);
 }
 void brojVeciOd99(int broj){
-    int temp;
-    temp = broj/100;
-    if(temp%2==0)digitalWrite(BD1, LOW);
-    else digitalWrite(BD1, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD2, LOW);
-    else digitalWrite(BD2, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD3, LOW);
-    else digitalWrite(BD3, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD4, LOW);
-    else digitalWrite(BD4, HIGH);
+    decToBin(broj/100);
     digitalWrite(katodaCifraTri, LOW);
     delay(1);
     digitalWrite(katodaCifraTri, HIGH);
-    
-    temp = (broj%100)/10;
-    if(temp%2==0)digitalWrite(BD1, LOW);
-    else digitalWrite(BD1, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD2, LOW);
-    else digitalWrite(BD2, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD3, LOW);
-    else digitalWrite(BD3, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD4, LOW);
-    else digitalWrite(BD4, HIGH);
+    decToBin((broj%100)/10);
     digitalWrite(katodaCifraDva, LOW);
     delay(1);
     digitalWrite(katodaCifraDva, HIGH);
-    
-    temp = broj%10;
-    if(temp%2==0)digitalWrite(BD1, LOW);
-    else digitalWrite(BD1, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD2, LOW);
-    else digitalWrite(BD2, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD3, LOW);
-    else digitalWrite(BD3, HIGH);
-    temp = temp /2;
-    if(temp%2==0)digitalWrite(BD4, LOW);
-    else digitalWrite(BD4, HIGH);
+    decToBin(broj%10);
     digitalWrite(katodaCifraJedan, LOW);
     delay(1);
     digitalWrite(katodaCifraJedan, HIGH);
